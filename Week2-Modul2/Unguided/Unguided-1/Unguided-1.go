cb5fbf64c359cc8664eac4da2bf021ebc392000a
sarahmaulidya <sarahmaulidya.n@gmail.com>
2026-03-07 15:49:57 +0700

    unguided

--- Week2-Modul2/Unguided/Unguided-1/Unguided-1.go
+++ Week2-Modul2/Unguided/Unguided-1/Unguided-1.go
@@ -4,8 +4,8 @@ func main() {
 	var tahun int
 	var status bool
 
-	fmt.Print("Tahun: ")
+	fmt.Print("Tahun : ")
 	fmt.Scan(&tahun)
 	status = (tahun % 400 == 0) || (tahun % 4 == 0 && tahun % 100 != 0)
-	fmt.Println("Kabisat: ", status)
+	fmt.Println("Kabisat :", status)
 }
