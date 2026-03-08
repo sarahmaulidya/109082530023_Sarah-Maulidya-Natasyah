cf19f2c1bc259ee3aa181ecce825a7d3a1afb837
sarahmaulidya <sarahmaulidya.n@gmail.com>
2026-03-08 22:36:13 +0700

    guided

--- Week2-Modul2/Guided/Guided-3/Guided-3.go
+++ Week2-Modul2/Guided/Guided-3/Guided-3.go
@@ -2,14 +2,14 @@ package main
 import "fmt"
 func main() {
 	var angka, pilihan int
-	
+
 	fmt.Println("-- MENU --")
 	fmt.Println("1. Cek angka == 10")
 	fmt.Println("2. Cek genap ganjil")
 	fmt.Print("Masukkan pilihan: ")
 	fmt.Scan(&pilihan)
 
-	switch pilihan{
+	switch pilihan {
 	case 1:
 		fmt.Print("Masukkan angka: ")
 		fmt.Scan(&angka)
