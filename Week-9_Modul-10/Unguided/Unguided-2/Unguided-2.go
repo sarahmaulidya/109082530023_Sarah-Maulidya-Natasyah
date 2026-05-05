3eb3edbecf2996059fe3377edd61d0d3220b3040
sarahmaulidya <sarahmaulidya.n@gmail.com>
2026-05-05 14:06:31 +0700

    modul 10

---
+++ Week-9_Modul-10/Unguided/Unguided-2/Unguided-2.go
@@ -0,0 +1,46 @@
+package main 
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+
+	var berat [1000]float64
+
+	for i := 0; i < x; i++ {
+		fmt.Scan(&berat[i])
+	}
+
+	var hasil [1000] float64
+	jumlahWadah := 0
+
+	i := 0
+	for i < x {
+		total := 0.0
+		count := 0
+
+		for j := 0; j < y && i < x; j++ {
+			total += berat[i]
+			i++
+			count++
+		}
+
+		hasil[jumlahWadah] = total
+		jumlahWadah++
+	}
+
+	sum := 0.0
+	for i := 0; i < jumlahWadah; i++ {
+		sum += hasil[i]
+	}
+
+	for i := 0; i < jumlahWadah; i++ {
+		fmt.Print(hasil[i], " ")
+	}
+	fmt.Println()
+
+	rata := sum / float64(jumlahWadah)
+
+	fmt.Println(rata)
+}
---
+++ Week-9_Modul-10/Unguided/Unguided-3/Unguided-3.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type arrBalita [100]float64
+
+func hitungMinMax(arrBerat arrBalita, n int, bMin, bMax *float64) {
+	*bMin = arrBerat[0]
+	*bMax = arrBerat[0]
+	for i := 1; i < n; i++ {
+		if arrBerat[i] < *bMin {
+				*bMin = arrBerat[i]
+		}
+		if arrBerat[i] > *bMax {
+			*bMax = arrBerat[i]
+		}
+	}
+}
+
+func rerata(arrBerat arrBalita, n int) float64 {
+	var total float64 = 0
+	for i := 0; i < n; i++ {
+		total += arrBerat[i]
+	}
+	return total / float64(n)
+}
+
+func main() {
+	var n int
+	var berat arrBalita
+	var bMin, bMax float64
+
+	fmt.Print("Masukkan banyak data berat balita: ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		fmt.Printf("Masukkan berat balita ke-%d: ", i + 1)
+		fmt.Scan(&berat[i])
+	}
+
+	hitungMinMax(berat, n, &bMin, &bMax)
+	rata := rerata(berat, n)
+
+	fmt.Printf("Berat balita minimum: %.2f kg\n", bMin)
+	fmt.Printf("Berat balita maksimum: %.2f kg\n", bMax)
+	fmt.Printf("Rerata berat balita: %.2f kg\n", rata)
+}
